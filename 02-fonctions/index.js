91cbb285bfb38db85a63fff11f2d89e5d5285231
Yasmine Attou <yasmine.attou@asdaex.com>
2025-10-07 21:29:02 +0200

    exos 02-fonctions faits

--- 02-fonctions/index.js
+++ 02-fonctions/index.js
@@ -8,4 +8,25 @@ function createUser(name,age=18,isActive=true) {
     };
 }
 
+console.log(createUser("Charlie", 30, false));
+
+
 //Exercice n°4: Fonctions pures
+function add(number1,number2) {
+    return number1+number2;
+}
+
+console.log(add(2,4));
+
+
+function multiply(number1,number2) {
+    return number1*number2;
+}
+
+console.log(multiply(2,4));
+
+function getFullName(firstName,lastName) {
+    return firstName +" " + lastName;
+}
+
+console.log(getFullName("yas","attou"));
