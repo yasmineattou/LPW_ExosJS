637b8ef3966a0a197bb483798b5d398296eb2dbf
yasmineattou <yasmine2000@hotmail.be>
2025-10-14 11:59:21 +0200

    04-boucles

---
+++ 04-boucles/index.js
@@ -0,0 +1,24 @@
+//Boucle for 
+let numbers1 =[];
+
+for (let i=1;i<=10;i++) {
+    numbers1.push(i);
+}
+console.log(numbers1);
+
+let numbers2 =[];
+for (let i=0;i<=20;i++) {
+    if (i%2==0) {
+            numbers2.push(i);
+    }
+}
+console.log(numbers2);
+
+let number=5;
+let result=1;
+
+for (let i=1;i<=number;i++) {
+    result*=i;
+}
+
+console.log(result);
